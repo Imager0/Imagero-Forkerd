0e6a51f8977e3866db2a83ad5aa28a275ec7333c
Artem Sidenko <137616254+Imager0@users.noreply.github.com>
2026-03-23 13:07:00 +0200

    Update renew_python_anywhere.py

--- renew_python_anywhere.py
+++ renew_python_anywhere.py
@@ -3,37 +3,6 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 import time
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-ACCOUNTS = [
-    {
-        "name": "Slaiz",
-        "username": os.environ.get('PA_USERNAME'),
-        "password": os.environ.get('PA_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/Slaiz/webapps/"
-    },
-    {
-        "name": "Anker",
-        "username": os.environ.get('ANKER_USERNAME'),
-        "password": os.environ.get('ANKER_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/ankermax/webapps/"
-    },
-    {
-        "name": "Akel",
-        "username": os.environ.get('PA_AKEL_USERNAME'),
-        "password": os.environ.get('PA_AKEL_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/akel/webapps/"
-    },
-    {
-        "name": "Imagero",
-        "username": os.environ.get('PA_IMAGERO_USERNAME'),
-        "password": os.environ.get('PA_IMAGERO_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/Imagero/webapps/"
-    }
-]
 
 for acc in ACCOUNTS:
     if not acc["username"] or not acc["password"]:
