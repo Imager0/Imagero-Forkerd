7d92671a3ea8b88d30cd918ec43b180e3982499a
Artem Sidenko <137616254+Imager0@users.noreply.github.com>
2026-03-23 14:25:26 +0200

    Update Accounts.py

--- Accounts.py
+++ Accounts.py
@@ -5,24 +5,24 @@ A = [
         "name": "Slaiz",
         "username": os.environ.get('PA_USERNAME'),
         "password": os.environ.get('PA_PASSWORD'),
-        "dashboard_url": f"https://www.pythonanywhere.com/user/{PA_USERNAME}/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{os.environ.get('PA_USERNAME')}/webapps/"
     },
     {
         "name": "Anker",
         "username": os.environ.get('ANKER_USERNAME'),
         "password": os.environ.get('ANKER_PASSWORD'),
-        "dashboard_url": f"https://www.pythonanywhere.com/user/{ANKER_USERNAME}/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{os.environ.get('ANKER_USERNAME')}/webapps/"
     },
     {
         "name": "Akel",
         "username": os.environ.get('PA_AKEL_USERNAME'),
         "password": os.environ.get('PA_AKEL_PASSWORD'),
-        "dashboard_url": f"https://www.pythonanywhere.com/user/{PA_AKEL_USERNAME}/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{os.environ.get('PA_AKEL_USERNAME')}/webapps/"
     },
     {
         "name": "Imagero",
         "username": os.environ.get('PA_IMAGERO_USERNAME'),
         "password": os.environ.get('PA_IMAGERO_PASSWORD'),
-        "dashboard_url": f"https://www.pythonanywhere.com/user/{PA_IMAGERO_USERNAME}/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{os.environ.get('A_IMAGERO_USERNAME')}/webapps/"
     }
 ]
