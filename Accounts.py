7eac1561873285d78b12db8b4bb0e298fb0cadfa
Artem Sidenko <137616254+Imager0@users.noreply.github.com>
2026-03-23 13:09:45 +0200

    Update Accounts.py

--- Accounts.py
+++ Accounts.py
@@ -5,24 +5,24 @@ ACCOUNTS = [
         "name": "Slaiz",
         "username": os.environ.get('PA_USERNAME'),
         "password": os.environ.get('PA_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/Slaiz/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{PA_USERNAME}/webapps/"
     },
     {
         "name": "Anker",
         "username": os.environ.get('ANKER_USERNAME'),
         "password": os.environ.get('ANKER_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/ankermax/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{ANKER_USERNAME}/webapps/"
     },
     {
         "name": "Akel",
         "username": os.environ.get('PA_AKEL_USERNAME'),
         "password": os.environ.get('PA_AKEL_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/akel/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{PA_AKEL_USERNAME}/webapps/"
     },
     {
         "name": "Imagero",
         "username": os.environ.get('PA_IMAGERO_USERNAME'),
         "password": os.environ.get('PA_IMAGERO_PASSWORD'),
-        "dashboard_url": "https://www.pythonanywhere.com/user/Imagero/webapps/"
+        "dashboard_url": f"https://www.pythonanywhere.com/user/{PA_IMAGERO_USERNAME}/webapps/"
     }
 ]
